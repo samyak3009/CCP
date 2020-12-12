4c1f423396322e56799e56da747670a7bd20a284
samyak3009 <samyak3009@gmail.com>
2020-12-12 08:10:55 +0530

    JFP

--- mathematics/count_no_of_digit.java
+++ mathematics/count_no_of_digit.java
@@ -4,12 +4,6 @@ public class Main {
 	public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        // int count =0;
         System.out.println((int)Math.floor(Math.log10(t)+1));
-        // while(t>0){
-        	// count +=1;
-        	// t=t/10;
-        // }
-        // System.out.println(count);
     }
 }
