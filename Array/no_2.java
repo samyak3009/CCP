0aeb0af3d987ac8d14a8f73c4cde9fc6b1c8705c
samyak3009 <samyak3009@gmail.com>
2021-01-06 19:07:33 +0530

    lets start;

--- Array/no_2.java
+++ Array/no_2.java
@@ -3,6 +3,10 @@
 import java.util.*;
 import java.lang.Math;
 public class Main {
+	static class Pair{
+		int min;
+		int max;
+	}
 	public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
@@ -10,6 +14,15 @@ public class Main {
 		for(int i=0;i<n;i++){
 			x[i] = sc.nextInt();
 		}
-		
+		Pair maxmin = new Pair();
+		maxmin.min = x[0];
+		maxmin.max = x[0];
+		for(int i=1;i<n;i++){
+			if(maxmin.min>x[i])
+				maxmin.min = x[i];
+			if(maxmin.max<x[i])
+				maxmin.max = x[i];
+		}
+		System.out.print(maxmin.max+" "+maxmin.min);
 	}
 }
